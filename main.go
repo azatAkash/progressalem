fff33fbbdaac1fb4db6b3a0683a961b183272957
aakash <azat.akash@nu.edu.kz>
2024-10-13 12:36:26 +0500

    fix

--- main.go
+++ main.go
@@ -1,8 +1,19 @@
 package main
 
-func main() {
-	accessToken := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjoiYWFrYXNoIiwiZXhwIjoxNzI4NDAzMzk3fQ.32G5pFVb3Zwk-yEeCHhjPj_fX5sDuBd1OvE6DokGsnQ"
+import (
+	"log"
+	"os"
+)
 
+func main() {
+	accessToken := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjoiYWFrYXNoIiwiZXhwIjoxNzI4ODMwMjYxfQ.R7GGwa9Poe2ug7nLJy7ULDkTFWcNf05CoIhvvPnJQfs"
+	if len(os.Args) == 2 {
+		if len(os.Args[1]) == len(accessToken) {
+			accessToken = os.Args[1]
+		} else {
+			log.Fatal("inconsitent access token")
+		}
+	}
 	htmlContent := extractData(accessToken)
 
 	printExtracted(htmlContent)
